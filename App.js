23bf529ed5c701952197a3920a53fc16abe0230f
Tukkis <54272662+Tukkis@users.noreply.github.com>
2024-02-10 20:03:33 +0200

    add searchbar and tab navigation

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+//plaveholder
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//placeholder
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+
+const Tab = createBottomTabNavigator();
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ components/SearchBar.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+
+const SearchBarComponent = ({ onSearch }) => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearch = () => {
+    // Perform any search logic here based on the searchText
+    onSearch(searchText);
+  };
+
+  return (
+    <View>
+      <SearchBar
+        placeholder="Placeholder..."
+        onChangeText={(text) => setSearchText(text)}
+        value={searchText}
+        onEndEditing={handleSearch}
+        onSubmitEditing={handleSearch}
+      />
+    </View>
+  );
+};
+
+export default SearchBarComponent;
