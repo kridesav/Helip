7760e9940d222c4f6c7450f84fe774c561e2f3a0
kridesav <savolainenkristjan@gmail.com>
2024-03-25 13:25:50 +0200

    getIcons moved into separate component and added few icons+removed some

---
+++ assets/Icons/index.js
@@ -0,0 +1,43 @@
+import footballIcon from './football.png';
+import gymIcon from './gym.png';
+import swimmingIcon from './swimming-pool.png';
+import golfIcon from './golf.png';
+import basketballIcon from './basketball.png';
+import iceSkatingIcon from './iceskating.png';
+import runningIcon from './running.png';
+import tennisIcon from './tennis.png';
+import hockeyIcon from './hockey.png';
+import boxingIcon from './boxing.png';
+import volleyballIcon from './volleyball.png';
+import infoIcon from './info.png';
+import skateIcon from './skate.png';
+import danceIcon from './dance.png';
+import baseballIcon from './baseball.png';
+import motorsportsIcon from './motorsports.png';
+import petsIcon from './pets.png';
+import horseIcon from './horse.png';
+import binocularIcon from './binoculars.png';
+import defaultIcon from './default.png';
+
+export {
+  footballIcon,
+  gymIcon,
+  swimmingIcon,
+  golfIcon,
+  basketballIcon,
+  iceSkatingIcon,
+  runningIcon,
+  tennisIcon,
+  hockeyIcon,
+  boxingIcon,
+  volleyballIcon,
+  infoIcon,
+  skateIcon,
+  danceIcon,
+  baseballIcon,
+  motorsportsIcon,
+  petsIcon,
+  horseIcon,
+  binocularIcon,
+  defaultIcon,
+};
